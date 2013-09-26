17123864297c94847fa9d23cd79773eeda761ced
Louise Au Yeung <louise@eabx.org>
2013-09-26 12:17:36 -0400

    initcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealEstateTracker::Application.config.secret_key_base = '0f9b1420b48d6be7f7743ea38be42a77eb64b419c50321e8a40f1e7d6dcc3c781ace6ba62aa177a606a38bb4c574ad7bba443a0ee638630da41953a5af6f125a'
