c5f41c215d32b8ce48e13e190eeadf4108aba0c6
Louise Au Yeung <louise@eabx.org>
2013-09-26 12:27:54 -0400

    initcommit for create a building

---
+++ spec/features/record_a_building_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'user records a building', %Q{
+  As a real estate associate
+  I want to record a building
+  So that I can refer back to pertinent information
+} do
+
+# Acceptance Criteria:
+
+# * I must specify a street address, city, state, and postal code
+# * Only US states can be specified
+# * I can optionally specify a description of the building
+# * If I enter all of the required information in the required format, the building is recorded.
+# * If I do not specify all of the required information in the required formats, the building is not recorded and I am presented with errors
+# * Upon successfully creating a building, I am redirected so that I can record another building.
+
+  scenario ''
+
+
+
+
+
+
+end
